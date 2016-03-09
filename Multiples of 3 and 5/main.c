65835a5bf73613a95f25fe0b13cfcac608f19457
Daniel Pulley <dpulley@uic.edu>
2016-03-09 09:57:32 -0600

    Add more comments

--- Multiples of 3 and 5/main.c
+++ Multiples of 3 and 5/main.c
@@ -3,12 +3,16 @@
 // Date: 02/2/2016
 #include <stdio.h>
 
+// Main Function
 int main()
 {
+    // setup a counter
 	int i,count=0;
+    // Loop through 0 - 999
 	for(i = 0; i < 1000; i++)
-		if((i % 3) == 0 || (i % 5) == 0)
-			count += i;	
-
+		if((i % 3) == 0 || (i % 5) == 0) // if i mod 3 or i mod 5 equals 0 
+			count += i;	                // Then sum the multiples
+    // print out answer
 	printf("Answer: %d\n", count);
 }
+// End of Main Function
